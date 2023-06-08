9961c17c96370261d794f755529346a87b0dc092
João Victor <79715612+joaoafigueira@users.noreply.github.com>
2023-06-08 16:16:41 -0300

    feat: connection with mongodb

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
 
-app.set('view engine', 'ejs')
+mongoose.connect('mongodb://localhost/urlShortener', {
+    useNewUrlParser: true, useUnifiedTopology: true
+})
+
+
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
     res.render('index')
-})
+});
+
+app.post('/shortUrls', (req, res) => {
+
+});
 
 app.listen(process.env.PORT || 5000);
